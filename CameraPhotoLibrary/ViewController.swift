62e486572a01c3bd7733113eb597cff85034c933
doomout <winquake3@hanmail.net>
2022-12-28 20:32:59 +0900

    readme update

--- CameraPhotoLibrary/ViewController.swift
+++ CameraPhotoLibrary/ViewController.swift
@@ -1,8 +1,13 @@
 
 import UIKit
+import MobileCoreServices
+import UniformTypeIdentifiers
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    let imagePicker: UIImagePickerController! = UIImagePickerController()
+    var captureImage : UIImage! //촬영하거나 포토 라이브러리에서 불러온 사진을 저장할 함수
+    var videoURL : URL! //녹화한 비디오의 URL을 저장할 변수
+    var flagImageSave = false //이미지 저장 여부를 나타낼 변수
     
     @IBOutlet var imgView: UIImageView!
     
@@ -10,19 +15,101 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    //사진 촬영
     @IBAction func btnCaptureImageFromCamera(_ sender: UIButton) {
+        //카메라의 사용 가능 여부를 확인. 사용 가능할 때~
+        if (UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            flagImageSave = true //이미지 저장을 허용
+            imagePicker.delegate = self //이미지 피커의 델리데이트를 self 로 설정
+            imagePicker.sourceType = .camera //이미지 피커의 소스 타입을 camera로 설정
+            imagePicker.mediaTypes = [UTType.image.identifier as String] //미디어 타입 설정
+            imagePicker.allowsEditing = false //편집은 허용하지 않음
+            
+            //현재 뷰 컨트롤러를 이미지피커로 대체
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            myAlert("Camera inaccessable", message: "Application cannot access the camera.")
+        }
     }
-    
+    //사진 불러오기
     @IBAction func btnLoadImageFromLibrary(_ sender: UIButton) {
+        if (UIImagePickerController.isSourceTypeAvailable(.photoLibrary)) {
+            flagImageSave = false
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.mediaTypes = [UTType.image.identifier as String]
+            imagePicker.allowsEditing = true
+            
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            myAlert("Photo album inaccessable", message: "Application cannot access the photo album.")
+        }
     }
     
     
     @IBAction func btnRecordVideoFromCamera(_ sender: UIButton) {
+        //카메라의 사용 가능 여부를 확인. 사용 가능할 때~
+        if (UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            flagImageSave = true //이미지 저장을 허용
+            imagePicker.delegate = self //이미지 피커의 델리데이트를 self 로 설정
+            imagePicker.sourceType = .camera //이미지 피커의 소스 타입을 camera로 설정
+            imagePicker.mediaTypes = [UTType.image.identifier as String] //미디어 타입 설정
+            imagePicker.allowsEditing = false //편집은 허용하지 않음
+            
+            //현재 뷰 컨트롤러를 이미지피커로 대체
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            myAlert("Camera inaccessable", message: "Application cannot access the camera.")
+        }
     }
     
     @IBAction func btnLoadVideoFromLibrary(_ sender: UIButton) {
+        if (UIImagePickerController.isSourceTypeAvailable(.photoLibrary)) {
+            flagImageSave = false
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.mediaTypes = [UTType.movie.identifier  as String]
+            imagePicker.allowsEditing = false
+            
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            myAlert("Photo album inaccessable", message: "Application cannot access the photo album.")
+        }
     }
     
+    func myAlert(_ title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let action = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    //사진이나 비디오를 촬영하거나 포토 라이브러리에서 선택이 끝났을 떄 호출되는 함수
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let mediaType = info[UIImagePickerController.InfoKey.mediaType] as! NSString
+        
+        if mediaType.isEqual(to: UTType.image.identifier as NSString as String) {
+            captureImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            if flagImageSave {
+                UIImageWriteToSavedPhotosAlbum(captureImage, self, nil, nil)
+            }
+            
+            imgView.image = captureImage
+        } else if mediaType.isEqual(to: UTType.movie.identifier  as NSString as String) {
+            if flagImageSave {
+                videoURL = (info[UIImagePickerController.InfoKey.mediaURL] as! URL)
+                
+                UISaveVideoAtPathToSavedPhotosAlbum(videoURL.relativePath, self, nil, nil)
+            }
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    //사진이나 비디오를 찍지 않고 취소하거나 선택하지 않고 ㅊ취소할 떄 호출되는 함수
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
 
--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 카메라를 이용하여 찍은 사진이나 비디오는 포토 라이브러리에 저장되고 이렇게 저장된 사진이나 비디오를 포토 라이브러리에서 불러올 수 있다.        
 또한 사진을 편집한 후 포토 라이브러리에 저장할 수도 있다.  
 포토 라이브러리는 사진 앱의 앨범을 의미하기에 사진이나 비디오를 찍은 후 사진 앱의 앨범에서 확인할 수 있다.
+
+변경점 
+예제에 있는 kUTTypeImage는 15버전 부터는 사용하지 말라는 경고가 나온다. 
+'kUTTypeImage' was deprecated in iOS 15.0: Use UTTypeImage instead.     
+대신 UTTypeImage를 사용하라고 권장한다.  
+UTTypeImage의 사용법은 다음과 같다.  
+
+import UniformTypeIdentifiers 
+
+//이미지와 동영상을 나눠 놨다.
+이미지 일 때 : kUTTypeImage  -> UTType.image.identifier 
+동영상 일 때 : kUTTypeImage  -> UTType.movie.identifier 

29b6532e5a33ba702bfec56c89edbcaeec1acb51
doomout <winquake3@hanmail.net>
2022-12-28 11:56:54 +0900

    Initial Commit

---
+++ CameraPhotoLibrary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraPhotoLibrary
+//
+//  Created by 현구김 on 2022/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
